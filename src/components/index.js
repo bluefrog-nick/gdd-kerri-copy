b6a958b318ed1f55c43c84be634bb3d8b4006d14
Nick Hinzmann <29715424+bluefrog-nick@users.noreply.github.com>
2020-10-21 13:12:48 -0500

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,45 @@
+import ContentBlockTwo from './ContentBlockTwo';
+import ContentBlock from './ContentBlock';
+import CtaBlock from './CtaBlock';
+import CtaButtons from './CtaButtons';
+import DocsMenu from './DocsMenu';
+import DocsSubmenu from './DocsSubmenu';
+import FeaturesBlock from './FeaturesBlock';
+import Footer from './Footer';
+import Header from './Header';
+import HeroBlock from './HeroBlock';
+import Menu from './Menu';
+import Social from './Social';
+import Layout from './Layout';
+
+export {
+    ContentBlockTwo,
+    ContentBlock,
+    CtaBlock,
+    CtaButtons,
+    DocsMenu,
+    DocsSubmenu,
+    FeaturesBlock,
+    Footer,
+    Header,
+    HeroBlock,
+    Menu,
+    Social,
+    Layout
+};
+
+export default {
+    ContentBlockTwo,
+    ContentBlock,
+    CtaBlock,
+    CtaButtons,
+    DocsMenu,
+    DocsSubmenu,
+    FeaturesBlock,
+    Footer,
+    Header,
+    HeroBlock,
+    Menu,
+    Social,
+    Layout
+};
